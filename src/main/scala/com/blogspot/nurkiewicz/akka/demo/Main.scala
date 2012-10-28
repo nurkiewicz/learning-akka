c87485195d671a5cbf28f82edc83820fd1d86d3a
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-10-28 12:07:56 +0100

    Remote client on only one node

--- src/main/scala/com/blogspot/nurkiewicz/akka/demo/Main.scala
+++ src/main/scala/com/blogspot/nurkiewicz/akka/demo/Main.scala
@@ -1,8 +1,8 @@
 package com.blogspot.nurkiewicz.akka.demo
 
 import com.weiglewilczek.slf4s.Logging
-import java.util.concurrent.TimeUnit
 import akka.actor.{ActorSystem, Props}
+import util.concurrent.TimeUnit
 
 /**
  * @author Tomasz Nurkiewicz
@@ -10,7 +10,7 @@ import akka.actor.{ActorSystem, Props}
  */
 
 object Main extends App with Logging {
-	val system = ActorSystem("Akka")
+	val system = ActorSystem("RandomOrgSystem")
 	system.log.info("Started")
 	val randomOrgBuffer = system.actorOf(Props[RandomOrgBuffer], "randomOrg")
 
@@ -19,12 +19,12 @@ object Main extends App with Logging {
 
 	val scalaRandom = new scala.util.Random(random)
 
-	for(_ <- 1 to 100000) {
-		TimeUnit.MILLISECONDS.sleep(50);
+	for(_ <- 1 to 1000) {
+		TimeUnit.MILLISECONDS.sleep(50 + (math.random * 50).toInt)
 		val start = System.nanoTime()
-		random.nextInt(1000)
+		val rnd = random.nextInt(1000)
 		val end = System.nanoTime()
-		logger.info(((end - start) / 1000000.0).toString)
+		logger.info(((end - start) / 1000000.0).toString + ": " + rnd)
 	}
 
 	system.shutdown()
--- src/main/scala/com/blogspot/nurkiewicz/akka/demo/RandomOrgClient.scala
+++ src/main/scala/com/blogspot/nurkiewicz/akka/demo/RandomOrgClient.scala
@@ -2,7 +2,8 @@ package com.blogspot.nurkiewicz.akka.demo
 
 import akka.event.LoggingReceive
 import com.ning.http.client.{AsyncCompletionHandler, Response, AsyncHttpClient}
-import akka.actor.Actor
+import akka.actor.{ActorRef, Actor}
+import collection.mutable
 
 /**
  * @author Tomasz Nurkiewicz
@@ -15,6 +16,7 @@ case class RandomOrgServerResponse(randomNumbers: List[Int])
 class RandomOrgClient extends Actor {
 
 	val client = new AsyncHttpClient()
+	val waitingForReply = new mutable.Queue[(ActorRef, Int)]
 
 	override def postStop() {
 		client.close()
@@ -26,12 +28,23 @@ class RandomOrgClient extends Actor {
 
 	def receive = LoggingReceive {
 		case FetchFromRandomOrg(batchSize) =>
-			val curSender = sender
-			val url = "https://www.random.org/integers/?num=" + batchSize + "&min=0&max=65535&col=1&base=10&format=plain&rnd=new"
-			client.prepareGet(url).execute {
-				response: Response =>
-					val numbers = response.getResponseBody.lines.map(_.toInt).toList
-					curSender ! RandomOrgServerResponse(numbers)
+			waitingForReply += (sender -> batchSize)
+			if (waitingForReply.tail.isEmpty) {
+				sendHttpRequest(batchSize)
 			}
+		case response: RandomOrgServerResponse =>
+			waitingForReply.dequeue()._1 ! response
+			if (!waitingForReply.isEmpty) {
+				sendHttpRequest(waitingForReply.front._2)
+			}
+	}
+
+	private def sendHttpRequest(batchSize: Int) {
+		val url = "https://www.random.org/integers/?num=" + batchSize + "&min=0&max=65535&col=1&base=10&format=plain&rnd=new"
+		client.prepareGet(url).execute {
+			response: Response =>
+				val numbers = response.getResponseBody.lines.map(_.toInt).toList
+				self ! RandomOrgServerResponse(numbers)
+		}
 	}
 }
